4a54b18098883124eddb6fd7570008bb7d6214a2
Jonathan <jonathan.emminizer@yahoo.com>
2025-10-26 14:36:05 -0400

    Updated App.vue, changed px to rem, and adjusted widths, radius, font, etc.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,44 +3,57 @@ import logo from '#/open_sourcery.png'
 </script>
 
 <template>
-  <div class="navbar">
-    <div class="left">
-      <img :src="logo" alt="Open Sourcery Logo" class="Navbar_Logo" />
-      <h3>UMD OPEN SOURCERY</h3>
-    </div>
-    <div class="right">
-      <a href="#">About Us</a>
-      <a href="#">Projects</a>
-      <a href="#">Sponsors</a>
-      <a href="#">Join</a>
+  <div class="page">
+    <div class="navbar">
+      <div class="left">
+        <img :src="logo" alt="Open Sourcery Logo" class="Navbar_Logo" />
+        <h3>UMD OPEN SOURCERY</h3>
+      </div>
+      <div class="right">
+        <a href="#">About Us</a>
+        <a href="#">Projects</a>
+        <a href="#">Sponsors</a>
+        <a href="#">Join</a>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+
+.page {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
 .navbar {
+  width: calc(1308 / 1440 * 100vw); /* 1308/1440 of the viewport width */
   display: flex;
   justify-content: space-between;
   align-items: center;
 
-  background-color: #0f5079;
-  border: 1px solid #004f7a;
-  padding: 8px 40px;
-  border-radius: 8px 8px 8px 8px;
+  background-color: rgba(15, 80, 121, 79);
+  border: 0.0625rem solid #004f7a;
+  padding: 0.5rem 2.5rem;
+  border-radius: 0.5rem;
 
-  font-family: 'Inter', sans-serif;
-  z-index: 1000;
+  font-family: 'Inter';
+  z-index: 500;
+  font-size: 1.5rem;
 }
 
 .navbar .left {
   display: flex;
   align-items: center;
-  gap: 10px;
+  gap: 0.625rem;
+  font-weight: 600;
 }
 
 .navbar .right {
   display: flex;
-  gap: 24px;
+  gap: 1.5rem;
   align-items: center;
   flex-direction: row;
 }
@@ -51,6 +64,7 @@ import logo from '#/open_sourcery.png'
   font-size: 0.9rem;
   margin: 0;
 }
+
 .navbar a {
   color: white;
   text-decoration: none;
@@ -58,13 +72,14 @@ import logo from '#/open_sourcery.png'
   transition: opacity 0.2s ease;
   margin: 0;
 }
+
 .navbar a:hover {
   opacity: 0.7;
 }
 
 .Navbar_Logo {
-  width: 32px;
-  height: 32px;
+  width: 4.875rem;
+  height: 4.875rem;
   object-fit: contain;
 }
 </style>
