db0850007599292da523adc924b6d74da821a8ad
Jonathan <jonathan.emminizer@yahoo.com>
2025-11-02 16:35:57 -0500

    Added footer to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import logo from '#/open_sourcery.png'
+import discord from '#/Discord_Image.png'
+import github from '#/Github_Image.png'
+import instagram from '#/Instagram_Image.png'
+import mail from '#/Mail_Image.png'
 </script>
 
 <template>
@@ -16,11 +20,27 @@ import logo from '#/open_sourcery.png'
         <a href="#">Join</a>
       </div>
     </div>
+
+    <div class="footer">
+      <img :src="logo" alt="Open Sourcery Logo" class="Footer_Logo" />
+      <h3>OPEN<br>SOURCERY</h3>
+
+      <div class="socials">
+        <a href="https://github.com/Open-Sourcery-UMD"><img :src="github" alt="Github Logo"
+            class="Footer_GH_Logo" /></a>
+        <a href="https://www.instagram.com/umdopensourcery/"><img :src="instagram" alt="Instagram Logo"
+            class="Footer_IG_Logo" /></a>
+        <a href="https://discord.com/invite/BWvbpgskZT"><img :src="discord" alt="Discord Logo"
+            class="Footer_DISCORD_Logo" /></a>
+        <a href="mailto:umdopensourcery@gmail.com"><img :src="mail" alt="Email" class="Footer_EMAIL_Logo" /></a>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=K2D&display=swap');
 
 .page {
   display: flex;
@@ -29,7 +49,9 @@ import logo from '#/open_sourcery.png'
 }
 
 .navbar {
-  width: calc(1308 / 1440 * 100vw); /* 1308/1440 of the viewport width */
+  width: calc(1308 / 1440 * 100vw);
+  /* 1308/1440 of the viewport width */
+  height: 5.313rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -79,6 +101,86 @@ import logo from '#/open_sourcery.png'
 .Navbar_Logo {
   width: 4.875rem;
   height: 4.875rem;
+  object-fit: contain;
+}
+
+
+.footer {
+  position: relative;
+  min-height: 20.125rem;
+  /* if you need that Figma height */
+  padding: 2rem 3rem;
+}
+
+.footer {
+  position: absolute;
+  /* Stay at the bottom of the screen */
+  bottom: 0;
+  /* Place it at the bottom */
+  left: 0;
+  /* Align it with the left edge */
+  width: 100%;
+  /* Full width of the screen */
+  height: 20.125rem;
+  /* Fixed height */
+  display: flex;
+  /* Flexbox to position children */
+  align-items: center;
+  /* Vertically center content */
+  gap: 1rem;
+  /* Space between items */
+  justify-content: flex-start;
+  /* Align content to the left */
+  font-family: 'K2D';
+  /* Custom font */
+  padding: 2rem 3rem;
+  /* Padding */
+  background-color: #075180;
+  /* Footer background color */
+  box-sizing: border-box;
+  /* Include padding in width calculation */
+}
+
+.socials {
+  margin-left: auto;
+  /* pushes to the right */
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+}
+
+.footer h3 {
+  font-size: 4rem;
+  color: white;
+}
+
+.Footer_Logo {
+  width: 16.5rem;
+  height: auto;
+  object-fit: contain;
+}
+
+.Footer_GH_Logo {
+  width: 6.188rem;
+  height: auto;
+  object-fit: contain;
+}
+
+.Footer_IG_Logo {
+  width: 6.188rem;
+  height: auto;
+  object-fit: contain;
+}
+
+.Footer_DISCORD_Logo {
+  width: 6.188rem;
+  height: auto;
+  object-fit: contain;
+}
+
+.Footer_EMAIL_Logo {
+  width: 6.188rem;
+  height: auto;
   object-fit: contain;
 }
 </style>
