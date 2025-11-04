fe0895a9eefff68c7887aec2536b26f370c23e81
Jonathan <jonathan.emminizer@yahoo.com>
2025-11-04 13:22:09 -0500

    Added SectionTitle.vue, included section title for About Us in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
+import SectionTitle from '@/components/SectionTitle.vue'
 </script>
 
 <template>
   <div class="page">
     <Navbar></Navbar>
+    <SectionTitle title="About Us"></SectionTitle>
     <Footer></Footer>
   </div>
 </template>
---
+++ frontend/src/components/SectionTitle.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const { title } = defineProps<{ title: string }>()
+</script>
+
+<template>
+    <div class="section_title">
+        <h2>{{ title }}</h2>
+        <div class="underline"></div>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+
+.section_title {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* centers children horizontally */
+  margin: 3rem 0 2rem 0;
+  text-align: center;
+  font-family: "Inria Sans", sans-serif;
+  opacity: 1;
+}
+
+.section_title h2 {
+  font-size: 6rem;
+  font-weight: 400;
+  margin: 0;
+  color: #1369A0;
+}
+
+.underline {
+  width: 447px;
+  height: 10px;
+  background-color: #1369A0;
+  margin-top: 0.5rem;
+  border-radius: 10px;
+}
+</style>
